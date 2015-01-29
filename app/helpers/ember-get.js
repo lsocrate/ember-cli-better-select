b7e9250d914152e42bbb1489683393d84eb72e00
nishi13 <augusto.nishi@gmail.com>
2015-01-29 21:18:58 -0200

    Fix issue when setting value with Null

--- app/helpers/ember-get.js
+++ app/helpers/ember-get.js
@@ -2,5 +2,5 @@ import Ember from 'ember';
 
 export default Ember.Handlebars.makeBoundHelper(function (object, key, sub) {
   if (typeof sub !== 'string' && typeof sub !== 'number') sub= '';
-  return Ember.get(object, key) || sub;
+  return (object && Ember.get(object, key)) || sub;
 });
